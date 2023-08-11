Should now work for arbitrary number of processes and arbitrary number of configurations per process. But it turns out that this is wrong, Something is rotten in the calculation of error bars. This must be fixed rapidly.

anderson/propagation.py
@@ -677,7 +677,7 @@ class Spectral_function:
     self.tab_spectrum2 /= n
     return
 
-  def mpi_merge(self, comm, timing):
+  def mpi_merge(self, comm, n_config, timing):
     start_mpi_time = timeit.default_timer()
     try:
       from mpi4py import MPI
@@ -686,18 +686,28 @@ class Spectral_function:
       return
     nprocs = comm.Get_size()
     rank = comm.Get_rank()
-#    print('Inside mpi_merge: nprocs =', nprocs)
-    toto = np.empty_like(self.tab_spectrum)
-    comm.Reduce(self.tab_spectrum,toto)
-    self.tab_spectrum = np.copy(toto)/nprocs
+#    print('Inside mpi_merge: nprocs =', nprocs),
+#    print('Inside mpi_merge process : ',rank,'tab_spectrum = ',self.tab_spectrum[220],self.tab_spectrum2[220])
 # At this stage, self.tab_spectrum2 is already the error bar over n_config configurations, computed by each process
-# One first has to go back to raw data
-    self.tab_spectrum2 = self.tab_spectrum2**2*nprocs
-    toto = np.empty_like(self.tab_spectrum2)
-    comm.Reduce(self.tab_spectrum2+nprocs**2*self.tab_spectrum**2,toto)
-    self.tab_spectrum2 = np.sqrt(np.copy(toto/nprocs))
-    print('Inside mpi_merge process : ',rank, 'nprocs = ',nprocs,'tab_spectrum = ',self.tab_spectrum[220:223])
-    timing.MPI_TIME+=(timeit.default_timer() - start_mpi_time)
+# First thing to do: starting from tab_spectrum and tab_spectrum2, reconstruct for eacy process the partial sums
+    self.tab_spectrum2 = n_config*self.tab_spectrum2**2+n_config**2*self.tab_spectrum**2
+    self.tab_spectrum *= n_config
+# Now compute the average spectrum by summing over processes
+    toto = np.empty_like(self.tab_spectrum)
+#    comm.Allreduce(self.tab_spectrum,toto)
+    self.tab_spectrum = np.copy(toto)/nprocs
+#    print('Toto inside mpi_merge process : ',rank,'toto = ',toto[220])
+#    self.tab_spectrum = np.copy(toto)/nprocs
+    toto2 = np.empty_like(self.tab_spectrum2)
+    comm.Allreduce(self.tab_spectrum2,toto2)
+    self.tab_spectrum = np.copy(toto)/(nprocs*n_config)
+    comm.Allreduce(self.tab_spectrum2,toto2)
+    self.tab_spectrum2 = np.copy(toto2)/(nprocs*n_config) - self.tab_spectrum**2/(nprocs*n_config)
+#    print('Second Inside mpi_merge process : ',rank,'tab_spectrum = ',self.tab_spectrum[220],' tab_spectrum2 =',self.tab_spectrum2[220])
+#    print('Third Inside mpi_merge process : ',rank,'tab_spectrum = ',self.tab_spectrum[220], self.tab_spectrum2[220],toto[220])
+#     self.tab_spectrum2 = np.sqrt(toto2/nprocs - self.tab_spectrum**2/nprocs)
+    self.tab_spectrum2 = np.copy((n_config*self.tab_spectrum2-(n_config*self.tab_spectrum)**2))/(n_config*nprocs)
+#    print('Inside mpi_merge process : ',rank, 'nprocs = ',nprocs,'tab_spectrum = ',self.tab_spectrum[220],self.tab_spectrum2[220])
     return
 
 

spectral_function/compute_spectral_function.py
@@ -126,19 +126,22 @@ def main():
 # Compute the spectral function and accumulate it, together with its square in order to have error bars
     partial_spectral_function = spectral_function.compute_spectral_function(i+rank*n_config, geometry, initial_state, H, my_timing)
 #    print('i=',i, 'partial spectral function = ',partial_spectral_function[105:108],'\n')
-    print('process ',rank,' config ',i,':', partial_spectral_function[220])
+#    print('process ',rank,' config ',i,':', partial_spectral_function[220])
     spectral_function.tab_spectrum += partial_spectral_function
+#    print('partial tab_spectrum = ',partial_spectral_function[220])
     spectral_function.tab_spectrum2 += partial_spectral_function**2
+#    print('intermediate tab_spectrum = ',spectral_function.tab_spectrum[220],spectral_function.tab_spectrum2[220])
 
 # normalize spectral_function.tab_spectrum and put the error bar in spectral_function.tab_spectrum2
+
+  print('quasi final tab_spectrum = ',spectral_function.tab_spectrum[220], (spectral_function.tab_spectrum2[220]/n_config-(spectral_function.tab_spectrum[220]/n_config)**2))
+  spectral_function.tab_spectrum2 = np.sqrt(spectral_function.tab_spectrum2/n_config-(spectral_function.tab_spectrum/n_config)**2)
   spectral_function.tab_spectrum /= n_config
-  spectral_function.tab_spectrum2 /= n_config
-  spectral_function.tab_spectrum2 = np.sqrt((spectral_function.tab_spectrum2-spectral_function.tab_spectrum**2)/n_config)
 #  spectral_function.tab_spectrum2 -= (spectral_function.tab_spectrum**2)/n_config
 #  print(spectral_function.tab_spectrum[0:3])
 #  print('i=',i,'cumulative spectral_function and square',spectral_function.tab_spectrum[105:108],spectral_function.tab_spectrum2[105:108]-spectral_function.tab_spectrum[105:108]**2,'\n')
   if mpi_version:
-    spectral_function.mpi_merge(comm,my_timing)
+    spectral_function.mpi_merge(comm,n_config,my_timing)
   t2 = time.perf_counter()
   my_timing.TOTAL_TIME = t2-t1
   if mpi_version: